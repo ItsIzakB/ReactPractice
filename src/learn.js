057a94b80f1d4bd2233a7a784f0d08d851d77705
izak <kazipriom@gmail.com>
2024-12-15 16:53:07 -0600

    anon funcs

--- src/learn.js
+++ src/learn.js
@@ -7,3 +7,14 @@ export default function findSum(a, b){
 export const findSum = (a,b) => {
     return a + b
 }
+
+// Anon funcs
+
+<button onClick = {() => {
+
+    console.log("hello world")
+}}>
+
+
+
+</button>
