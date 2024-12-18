fc945908c116b509261e45681ce6d224f043d082
izak <kazipriom@gmail.com>
2024-12-17 19:41:09 -0600

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./styles.css"
 export default App(){
   return <form classNames = "new-item-form">
-  <div className="form-row">
-  <label htmlFor="item">New Item</label>
-  <input type="text"></input>
-  </div>
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text"></input>
+    </div>
   </form>
 }
