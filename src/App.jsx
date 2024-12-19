757f6560a631796aab3a6a7494f35385bc6cd8ee
izak <kazipriom@gmail.com>
2024-12-18 18:24:36 -0600

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./styles.css"
 export default function App(){
-  return <form classNames = "new-item-form">
+  return (
+  <form classNames = "new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id ="item"></input>
+      <input type="text" id ="item" />
     </div>
+    <button className="btn">Add</button>
   </form>
+  )
 }
