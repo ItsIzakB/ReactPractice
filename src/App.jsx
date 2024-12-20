37dcea54d2b458cdb2d5a07ddbb5d5b92ed8dd67
izak <kazipriom@gmail.com>
2024-12-19 23:19:06 -0600

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App(){
   <form classNames = "new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id ="item" />
+      <input value = {newItem} type="text" id ="item" />
     </div>
     <button className="btn">Add</button>
   </form>
