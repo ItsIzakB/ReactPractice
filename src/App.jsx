4237f09a495c6cb79a038c1b068ab52acce517b2
izak <kazipriom@gmail.com>
2024-12-21 19:09:31 -0600

    using tutorial source code because I can't find the bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,29 @@
-import {useState} from "react"
-import "./styles.css"
-import {NewTodoForm} from "./NewTodoForm"
+import { useState } from "react"
 
-export default function App(){
-  const [todos, setTodos] = useState([])
+export function NewTodoForm({ onSubmit }) {
+  const [newItem, setNewItem] = useState("")
 
-  function toggleTodos (id, completed){
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id){
-          return {...todo, completed}
-        }
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (newItem === "") return
 
+    onSubmit(newItem)
 
-        return todo
-      })
-    })
+    setNewItem("")
   }
 
-
-  function deleteTodo(id){
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
-
-  } 
-
-  
   return (
-  <>
-
-  <NewTodoForm />
-  <h1 className="header">Todo List</h1>
-  <ul className="list">
-    {todos.length === 0 && "No Todos"}
-    {todos.map(todo => {
-      return(
-        <li key={todo.id}>
-          <label>
-            <input type = "checkbox" checked={todo.completed}
-          onChange={e =>toggleTodos(todo.id,e.target_checked)}/>
-          {todo.title}
-          </label>
-          <button onClick ={() => deleteTodo(todo.id)}
-                  className = "btn btn-danger">Delete</button>
-        </li>
- 
-
-      )
-    })}
- </ul>
-
-  </>
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
   )
 }
--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,6 +1,19 @@
   export function NewTodoForm(){
 
     const [newItem, setNewItem] = useState("")
+    function handleSubmit (e){
+        e.preventDefault();
+
+    //     setTodos(currentTodos => {
+
+    //     return [
+    //         ...currentTodos, {id: crypto.randomUUID, title: newItem, completed:
+    //         false},
+    //     ]
+    //     })
+
+    setNewItem("");
+
   }
  
     return(
