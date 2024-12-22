d38675745d975ce073740ffa244238691e63c042
izak <kazipriom@gmail.com>
2024-12-21 18:54:43 -0600

    newtodo

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,6 +1,24 @@
   export function NewTodoForm(){
 
     const [newItem, setNewItem] = useState("")
+
+
+   
+  function handleSubmit (e){
+    e.preventDefault();
+
+    setTodos(currentTodos => {
+
+      return [
+        ...currentTodos, {id: crypto.randomUUID, title: newItem, completed:
+        false},
+      ]
+    })
+
+    setNewItem("");
+
+  }
+ 
     return(
     <form onSubmit = {handleSubmit} className= "new-item-form">
         <div className="form-row">
