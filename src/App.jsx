d495b712d00f3c6e0dcb3e52f740a820a0f3f36e
izak <kazipriom@gmail.com>
2024-12-21 18:55:11 -0600

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import {useState} from "react"
 import "./styles.css"
 export default function App(){
-  const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
-
-
-
-  function handleSubmit (e){
-    e.preventDefault();
-
-    setTodos(currentTodos => {
-
-      return [
-        ...currentTodos, {id: crypto.randomUUID, title: newItem, completed:
-        false},
-      ]
-    })
-
-    setNewItem("");
-
-  }
 
   function toggleTodos (id, completed){
     setTodos(currentTodos => {
