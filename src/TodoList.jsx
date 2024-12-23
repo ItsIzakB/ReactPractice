692f61398a81ebdb4ca403b72b226aefea34ca34
izak <kazipriom@gmail.com>
2024-12-22 20:25:55 -0600

    TODO LIST

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,5 +1,22 @@
 export default TodoList(id){
 
-    return 
+    return  <ul className="list">
+    {todos.length === 0 && "No Todos"}
+    {todos.map(todo => {
+      return(
+        <li key={todo.id}>
+          <label>
+            <input type = "checkbox" checked={todo.completed}
+          onChange={e =>toggleTodos(todo.id,e.target_checked)}/>
+          {todo.title}
+          </label>
+          <button onClick ={() => deleteTodo(todo.id)}
+                  className = "btn btn-danger">Delete</button>
+        </li>
+ 
+
+      )
+    })}
+ </ul> 
 
 }
