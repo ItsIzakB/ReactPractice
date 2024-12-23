6564ff600273af919be55be5b1db5a31bf0289f1
izak <kazipriom@gmail.com>
2024-12-22 20:42:02 -0600

    TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -2,11 +2,11 @@ export function TodoItem(){
     <li>
         <label>
             <input
-                type="checkbox", checked={todo.complete}
-            ></input>
-
-
-
+                type="checkbox"
+                checked={todo.complete}
+                
+            />
+            {todo.title}
         </label>
 
 
