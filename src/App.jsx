de2db3aadeb143297b458e92e130b1238ffe79d7
izak <kazipriom@gmail.com>
2024-12-23 19:41:34 -0600

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App(){
 
   <NewTodoForm />
   <h1 className="header">Todo List</h1>
-  <TodoList todos={todos}/>
+  <TodoList todos={todos} toggleTodo = {toggleTodo}/>
 
 
   </>
