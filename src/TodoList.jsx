20711fbae20700e6ea55ab175d3e22f5d608e052
izak <kazipriom@gmail.com>
2024-12-23 19:40:44 -0600

    TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -6,7 +6,7 @@ export function TodoList({ todos, toggleTodo, deleteTodo }) {
         {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
-            <TodoItem {...todo} key={todo.id}
+            <TodoItem {...todo} key={todo.id} toggleTodo ={toggleTodo} deleteTodo={deleteTodo}
             />
           )
         })}
